caed2b2ac78171f9110ab0b7a6cdecb29f4991cc
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-04-14 17:25:31 +0200

    [add] funzione per aggiungere la task

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,17 +41,31 @@ createApp({
         },
       ],
       errorMsg : '',
+      newTaskText : ''
     }
   },
 
   methods : {
     deleteTask(index){
-      // errorMsg = ''
+      errorMsg = ''
       if(this.tasks[index].done){
         this.tasks.splice(index, 1)
       }else{
         this.errorMsg = 'Attenzione per cancellare la task assicurati di averla completata!'
       }
+    },
+
+    addTask(){
+      if(this.newTaskText.length > 4){
+        const newTask = {
+          text : this.newTaskText,
+          done : false,
+        }
+        this.tasks.unshift(newTask)
+      }else {
+        this.errorMsg = 'Il testo deve contenere almeno 5 caratteri'
+      }
+      this.newTaskText = ''
     }
   }
 }).mount('#app')
