411eb8ddf236f3a1e315825716af14cd4361dd51
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-04-14 16:34:08 +0200

    [add] inizio aggiunta vue

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,29 @@ Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando s
 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 2- cliccando sul testo dell’item, invertire il valore della proprietà done del todo corrispondente (se `done` era uguale a `false`, impostare `true` e viceversa)
 */
+
+const {createApp} = Vue;
+
+createApp({
+  data(){
+    return{
+      tasks : [
+
+        {
+          text : 'fare la spesa',
+          done : false
+        },
+
+        {
+          text : "bagnare l'orto",
+          done : false
+        },
+
+        {
+          text : 'chiamare avvocato',
+          done : false
+        },
+      ]
+    }
+  }
+}).mount('#app')
